24d7016e182ef4f186790258ad9fee03458883c7
lailabn <benalilaila1119@gmail.com>
2026-06-06 01:32:49 +0100

    Change apiUrl to relative path for deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api' // L'URL de ton serveur une fois déployé
+  apiUrl: '/api' // L'URL de ton serveur une fois déployé
 };
